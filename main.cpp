373bae57603842c3c5dd96cffead8d67d42dc2d4
ryusei9 <k023g0066@g.neec.ac.jp>
2024-04-23 18:36:02 +0900

    MT3_00_05

--- main.cpp
+++ main.cpp
@@ -1,12 +1,48 @@
 #include <Novice.h>
+#include <cmath>
 
-const char kWindowTitle[] = "LC1C_17_サトウ_リュウセイ_タイトル";
+const char kWindowTitle[] = "LE2B_13_サトウ_リュウセイ_MT3_00_05";
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3 translate);
+
+// x軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian);
+
+// y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian);
+
+// z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+// 4x4行列の数値表示
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Vector3 scale{ 1.2f,0.79f,-2.1f };
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Vector3 translate{ 2.7f,-4.15f,1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -32,7 +68,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
+		MatrixScreenPrintf(0, 0, worldMatrix);
 		///
 		/// ↑描画処理ここまで
 		///
@@ -49,4 +85,80 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
+}
+
+// アフィン変換
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3 translate){
+	Matrix4x4 resultAffineMatrix = {};
+	Matrix4x4 resultRotateXYZMatrix = Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
+	resultAffineMatrix.m[0][0] = scale.x * resultRotateXYZMatrix.m[0][0];
+	resultAffineMatrix.m[0][1] = scale.x * resultRotateXYZMatrix.m[0][1];
+	resultAffineMatrix.m[0][2] = scale.x * resultRotateXYZMatrix.m[0][2];
+	resultAffineMatrix.m[1][0] = scale.y * resultRotateXYZMatrix.m[1][0];
+	resultAffineMatrix.m[1][1] = scale.y * resultRotateXYZMatrix.m[1][1];
+	resultAffineMatrix.m[1][2] = scale.y * resultRotateXYZMatrix.m[1][2];
+	resultAffineMatrix.m[2][0] = scale.z * resultRotateXYZMatrix.m[2][0];
+	resultAffineMatrix.m[2][1] = scale.z * resultRotateXYZMatrix.m[2][1];
+	resultAffineMatrix.m[2][2] = scale.z * resultRotateXYZMatrix.m[2][2];
+	resultAffineMatrix.m[3][0] = translate.x;
+	resultAffineMatrix.m[3][1] = translate.y;
+	resultAffineMatrix.m[3][2] = translate.z;
+	resultAffineMatrix.m[3][3] = 1;
+	return resultAffineMatrix;
+}
+
+// x軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 resultRotateXMatrix = {};
+	resultRotateXMatrix.m[0][0] = 1;
+	resultRotateXMatrix.m[1][1] = std::cos(radian);
+	resultRotateXMatrix.m[1][2] = std::sin(radian);
+	resultRotateXMatrix.m[2][1] = -std::sin(radian);
+	resultRotateXMatrix.m[2][2] = std::cos(radian);
+	resultRotateXMatrix.m[3][3] = 1;
+	return resultRotateXMatrix;
+}
+
+// y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 resultRotateYMatrix = {};
+	resultRotateYMatrix.m[0][0] = std::cos(radian);
+	resultRotateYMatrix.m[0][2] = -std::sin(radian);
+	resultRotateYMatrix.m[1][1] = 1;
+	resultRotateYMatrix.m[2][0] = std::sin(radian);
+	resultRotateYMatrix.m[2][2] = std::cos(radian);
+	resultRotateYMatrix.m[3][3] = 1;
+	return resultRotateYMatrix;
+}
+
+// z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 resultRotateZMatrix = {};
+	resultRotateZMatrix.m[0][0] = std::cos(radian);
+	resultRotateZMatrix.m[0][1] = std::sin(radian);
+	resultRotateZMatrix.m[1][0] = -std::sin(radian);
+	resultRotateZMatrix.m[1][1] = std::cos(radian);
+	resultRotateZMatrix.m[2][2] = 1;
+	resultRotateZMatrix.m[3][3] = 1;
+	return resultRotateZMatrix;
+}
+
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 resultMultiply = {};
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			resultMultiply.m[i][j] = m1.m[i][0] * m2.m[0][j] + m1.m[i][1] * m2.m[1][j] + m1.m[i][2] * m2.m[2][j] + m1.m[i][3] * m2.m[3][j];
+		}
+	}
+	return resultMultiply;
+}
+
+// 4x4行列の数値表示
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
 }
